ada8b280dce10b3f5b0ebd08598471041d900b14
Maureen Vanhoren <maureenvanhoren@gmail.com>
2022-01-27 14:54:13 +0100

    exo2

--- exos/src/App.js
+++ exos/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { nanoid } from 'nanoid';
+import { nanoid } from 'nanoid'; // npm i nanoid
 import Ex01 from './components/ex01/ex01';
 import Ex02 from './components/ex02/ex02';
 
@@ -8,7 +8,8 @@ function App() {
     { id: nanoid(), nom: 'TV 4k', prix: 500, promo: false },
     { id: nanoid(), nom: 'Machine à laver', prix: 100, promo: true },
     { id: nanoid(), nom: 'PlayStation 4', prix: 300, promo: false },
-    { id: nanoid(), nom: 'Four à micro-ondes', prix: 60, promo: true }
+    { id: nanoid(), nom: 'Four à micro-ondes', prix: 60, promo: true },
+    { id: nanoid(), nom: 'Frigo américain', prix: 450, promo: true }
   ]
 
   return (
--- exos/src/components/ex02/ex02.jsx
+++ exos/src/components/ex02/ex02.jsx
@@ -1,23 +1,42 @@
 import style from './ex02.module.css';
 import PropTypes from 'prop-types';
 
-const Product = (props) => {
-    const {nom, prix, promo} = props;
+const Product = ({nom, prix, promo}) => (
+    // pas de code ici, donc c'est simple et on peut en faire une lambda et enlever le return
+    <tr className={style.trPromo}>
+        <td className={style.nomProduit}>
+            {nom}
+        </td>
+        <td className={style.prixProduit}>
+            < DisplayPrice value={prix} />
+        </td>
+        <td className={style.promo}>
+            {(promo === true) ? 'promo' : ''}
+        </td>
+    </tr>
+)
+
+const DisplayPrice = (props) => {
+    // un constructeur permettant de créer des objets pour formater des nombres en fonction de la locale
+    // https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat
+    const prix = new Intl.NumberFormat('fr-BE', { style: 'currency', currency: 'EUR' }).format(props.value)
 
     return (
-        <tr>
-            <td className={style.nomProduit}>
-                {nom}
-            </td>
-            <td>
-                € {prix}
-            </td>
-            <td className={style.promo}>
-                {(promo === true) ? promo : ''}
-            </td>
-        </tr>
+        <span>{prix}</span>
     )
 }
+
+DisplayPrice.propTypes = {
+    value: PropTypes.number.isRequired
+}
+
+// on divise la table en autant de parties qu'on veut, ce qu'on trouve facile
+const TableHead = (props) => (
+    <thead className={style.tabHead}>
+        <th>Nom du produit</th>
+        <th colSpan={2}>Prix</th>
+    </thead>
+)
 
 const ProductList = (props) => {
     const productsJSX = props.products.map(
@@ -25,7 +44,8 @@ const ProductList = (props) => {
     )
 
     return (
-        <table>
+        <table className={style.tabPromo}>
+            < TableHead />
             <tbody>
                 {productsJSX}
             </tbody>
@@ -38,8 +58,9 @@ ProductList.defaultProps = {
 }
 
 ProductList.propTypes = {
-    nom: PropTypes.string,
-    prix: PropTypes.number
+    nom: PropTypes.string.isRequired,
+    prix: PropTypes.number.isRequired,
+    promo: PropTypes.bool.isRequired
 }
 
 export default ProductList;
